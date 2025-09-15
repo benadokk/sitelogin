7331ce6cd8b68bdd4f64fcd30b9d99538ae53d78
benadokk <ber.oliveira08@gmail.com>
2025-09-15 10:50:23 -0300

    Add files via upload

---
+++ sitelogin/code/script.js
@@ -0,0 +1,6 @@
+    document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  // Simulação de login
+  window.location.href = "home.html"; 
+});
